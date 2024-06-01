8b3d1d1ef32758b6b6017216421c0754ba4df6ab
weirdrazak <rappiah316@st.ug.edu.gh>
2024-06-01 10:00:45 +0000

    Added Ticket Price consle application

---
+++ TicketCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            double ticketPrice;
+            if (age >= 65 || age <= 12)
+            {
+                ticketPrice = 7.0;
+            }
+            else
+            {
+                ticketPrice = 10.0;
+            }
+
+            Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+        }
+    
+    }
